1cc4a3688424ab25f152fb5db8814c4924614c28
Kelli Parris <kelli@hsdesignhouse.com>
2017-12-15 14:13:47 -0600

    backtrack

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
 
 	def chatroom
 		session[:conversations] ||= []
+
     @users = User.all.where.not(id: current_or_guest_user)
     @conversations = Conversation.includes(:recipient, :messages)
                                  .find(session[:conversations])
@@ -9,6 +10,7 @@ class StaticPagesController < ApplicationController
 
 	def support
 		session[:customer_chats] ||= []
+
     @users = User.all.where.not(id: current_or_guest_user)
     @customer_chats = CustomerChat.includes(:recipient, :notes)
                                  		.find(session[:customer_chats])
