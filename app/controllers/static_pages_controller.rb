ca357eb249583851910aa56be645b2487908457d
Kelli Parris <kelli@hsdesignhouse.com>
2017-12-18 09:50:11 -0600

    moved admin user list if state. to controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,9 +3,14 @@ class StaticPagesController < ApplicationController
 	def chatroom
 		session[:conversations] ||= []
 
-    @users = User.all.where.not(id: current_or_guest_user)
     @conversations = Conversation.includes(:recipient, :messages)
                                  .find(session[:conversations])
+
+    if current_or_guest_user.admin?
+	    @users = User.all.where.not(id: current_or_guest_user)
+	  else
+	    @users = User.all.where(admin: true)
+    end
 	end
 
 	def support
